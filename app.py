c427edcbbe556e4e94c458ab3aecabaf8cf0e575
tkoar <terrancekoar@gmail.com>
2018-05-29 15:13:22 -0400

    removed soltution

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 # import Flask and jsonify from flask
-# import SQLAlchemy from flask_sqlalchemy
 from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
+# import SQLAlchemy from flask_sqlalchemy
+
 
 # initialize new flask app
 app = Flask(__name__)
-# add configurations and database
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+# add configurations and database URI
+
+
 # connect flask_sqlalchemy to the configured flask app
-db = SQLAlchemy(app)
+
+
 
 # create models for application
 class User(db.Model):
@@ -32,45 +33,4 @@ class Tweet(db.Model):
         return tweet
 
 # define routes and their respective functions that return the correct data
-@app.route('/api/users')
-def user_index():
-    all_users = User.query.all()
-    all_users_dicts = [user.to_dict() for user in all_users]
-    return jsonify(all_users_dicts)
-
-@app.route('/api/users/<int:id>')
-def find_user(id):
-    user = User.query.filter(User.id == id).first()
-    return jsonify(user.to_dict())
-
-
-@app.route('/api/users/<name>')
-def find_user_by_name(name):
-    user = User.query.filter(User.username.like(name)).first()
-    return jsonify(user.to_dict())
-
-@app.route('/api/tweets')
-def tweet_index():
-    all_tweets = Tweet.query.all()
-    all_tweets_dicts = [tweet.to_dict() for tweet in all_tweets]
-    return jsonify(all_tweets_dicts)
-
-@app.route('/api/tweets/<int:id>')
-def find_tweet(id):
-    return jsonify(Tweet.query.filter(Tweet.id == id).first().to_dict())
-
-@app.route('/api/users/<int:user_id>/tweets')
-def find_users_tweets(user_id):
-    return jsonify(User.query.filter(User.id == user_id).first().to_dict())
-
-@app.route('/api/users/<user_name>/tweets')
-def find_users_tweets_by_user_name(user_name):
-    return jsonify(User.query.filter(User.name == user_name.lower().title()).first().tweets())
-
-@app.route('/api/tweets/<int:tweet_id>/user')
-def find_tweets_user(tweet_id):
-    return jsonify(Tweet.query.filter(Tweet.id == tweet_id).first().user.to_dict())
-
-
-if __name__ == "__main__":
-    app.run()
+# remember that each function must have a unique name
